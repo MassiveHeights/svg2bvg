cfadc0f2b1e97d5d733aecb22bc98a379d4a6ddd
62316e <62316e@gmail.com>
2018-07-22 20:09:47 +0300

    minor refactoring

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-const svg2bsv = require('./lib/svg2bsv');
+const svg2bsg = require('./lib/svg2bvg');
 const fs = require('fs');
 const program = require('commander');
 
 
 program
   .version('0.1.0')
-  .usage('--input <file.svg> --output [file.bsv]')
+  .usage('--in <file.svg> --out [file.bvg]')
   .option('--in [in.svg]', 'Input file')
   .option('--out [out.bsv]', 'Output file')
   .parse(process.argv);
@@ -24,9 +24,9 @@ if (argc.rawArgs.length !== 6) {
 
 let svg = fs.readFileSync(program.in);
 
-let d = svg2bsv.convert(svg);
+let d = svg2bsg.convert(svg);
 let svgLen = svg.length;
 let bsvLen = JSON.stringify(d).length;
 
-console.log('SVG Size:', svgLen, '~ BSV Size:', bsvLen, '~ diff:', (svgLen / bsvLen).toFixed(2) + 'x smaller');
+console.log('SVG Size:', svgLen, 'bytes, BVG Size:', bsvLen, 'bytes. Diff:', (svgLen / bsvLen).toFixed(2) + 'x smaller');
 fs.writeFileSync(program.out, JSON.stringify(d/*, null, 2*/));
